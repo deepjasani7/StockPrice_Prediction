5c8de975fb3d450ef59a88c3b95675ee3db48042
deepjasani7 <jasanideep7789@gmail.com>
2025-02-19 16:59:29 +0530

    Stock_Analysis.py

--- Stock_Price_Pridiction/pages/Stock_Analysis.py
+++ Stock_Price_Pridiction/pages/Stock_Analysis.py
@@ -4,8 +4,10 @@ import yfinance as yf
 import plotly.graph_objects as go 
 import datetime
 import ta 
-from numpy import nan
 from pages.utils.plotly_figure import plotly_table, candlestick, close_chart, RSI, MACD, Moving_average
+
+import numpy as np
+NaN = np.nan
 
 st.set_page_config(
     page_title="Stock Analysis",
